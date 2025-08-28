3d526fc83b7ce3455e6213fd58832d83d68652b6
Jose Bordon <40216449@live.napier.ac.uk>
2025-08-28 13:30:59 +0100

    Sanitised script paths for public release

--- step2.sh
+++ step2.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #
-#Author: Jose Bordon
+# Author: Jose Bordon
 #
-#Description: step2.sh script. for loop implemented to
-#iterate over the files and create the book and append it
-#to the final file.
+# Description: step2.sh script.
+# Uses for loops to iterate over files, create the book, and append it to the final file.
 #
 
 echo "Beginning text creation HOUSE_OF_THE_DOCKERS.txt..."
@@ -12,98 +11,63 @@ sleep 1
 echo "Loading..."
 
 # First round
-# Loop through all .txt files in the dir starting with sorted_docker
-for i in  $(ls /home/student/j.bordon/sorted_docker*.txt); do
-        # Read the contents of the current file, take the first 2 lines,
-        # extract the last column using awk, and append it to temp.txt
-        cat $i | head -n 2 | awk '{print $NF}' >> temp.txt;
+for i in $(ls ~/projects/house_of_the_dockers/sorted_docker*.txt); do
+    cat $i | head -n 2 | awk '{print $NF}' >> temp.txt
 done
 
-# Read the contents of temp.txt and append to HOUSE_OF_THE_DOCKERS.txt
 for i in $(cat temp.txt); do 
-        cat $i >> HOUSE_OF_THE_DOCKERS.txt;
+    cat $i >> HOUSE_OF_THE_DOCKERS.txt
 done
-
-# Remove temporary file
 rm temp.txt
 
 # Second round
-# Loop through all .txt files in the dir starting with sorted_docker
-for i in  $(ls /home/student/j.bordon/sorted_docker*.txt); do
-        # Read the contents of the current file, take the first 4 lines,
-        # then select the last 2 lines, extract the last column using awk,
-        #  and append to temp.txt 
-        cat $i |head -n 4 | tail -n 2| awk '{print $NF}' >> temp.txt;
+for i in $(ls ~/projects/house_of_the_dockers/sorted_docker*.txt); do
+    cat $i | head -n 4 | tail -n 2 | awk '{print $NF}' >> temp.txt
 done
 
-# Read the contents of temp.txt and append to HOUSE_OF_THE_DOCKERS.txt
 for i in $(cat temp.txt); do 
-        cat $i >> HOUSE_OF_THE_DOCKERS.txt;
+    cat $i >> HOUSE_OF_THE_DOCKERS.txt
 done
-
-#Remove temporary file
 rm temp.txt
 
-# Third round
-# Loop through docker1.txt file 
-for i in  $(ls /home/student/j.bordon/sorted_docker1.txt);do
-        # Read the contents of the current file, take the first 5 lines, 
-        # then select the last line, extract the last column using awk,
-        # and append to temp.txt 
-        cat $i | head -n 5 | tail -n 1 | awk '{print $NF}' >> temp.txt;
+# Third round - docker1
+for i in $(ls ~/projects/house_of_the_dockers/sorted_docker1.txt); do
+    cat $i | head -n 5 | tail -n 1 | awk '{print $NF}' >> temp.txt
 done
 
-	# Read the contents of temp.txt and append to HOUSE_OF_THE_DOCKERS.txt
-	for i in $(cat temp.txt); do
-        	cat $i >> HOUSE_OF_THE_DOCKERS.txt;
-	done
-# Remove temporary file
+for i in $(cat temp.txt); do
+    cat $i >> HOUSE_OF_THE_DOCKERS.txt
+done
 rm temp.txt
 
-# Loop through docker2.txt file
-for i in  $(ls /home/student/j.bordon/sorted_docker2.txt);do
-        # Read the contents of the current file, take the first 6 lines, 
-        # then select the last 2 lines, extract the last column using awk,
-        # and append to temp.txt 
-        cat $i | head -n 6 | tail -n 2 | awk '{print $NF}' >> temp.txt;
+# Third round - docker2
+for i in $(ls ~/projects/house_of_the_dockers/sorted_docker2.txt); do
+    cat $i | head -n 6 | tail -n 2 | awk '{print $NF}' >> temp.txt
 done
 
-	# Read the contents of temp.txt and append to HOUSE_OF_THE_DOCKERS.txt
-	for i in $(cat temp.txt); do
-        	cat $i >> HOUSE_OF_THE_DOCKERS.txt;
-	done
-
-# Remove temporary file
+for i in $(cat temp.txt); do
+    cat $i >> HOUSE_OF_THE_DOCKERS.txt
+done
 rm temp.txt
 
-# Loop through docker3.txt file 
-for i in  $(ls /home/student/j.bordon/sorted_docker3.txt);do
-        # Read the contents of the current file, take the first 6 lines, 
-        # then select the last 2 lines, extract the last column using awk,
-        # and append to temp.txt 
-        cat $i | head -n 6 | tail -n 2 | awk '{print $NF}' >> temp.txt;
+# Third round - docker3
+for i in $(ls ~/projects/house_of_the_dockers/sorted_docker3.txt); do
+    cat $i | head -n 6 | tail -n 2 | awk '{print $NF}' >> temp.txt
 done
 
-	# Read the contents of temp.txt and append to HOUSE_OF_THE_DOCKERS.txt
-	for i in $(cat temp.txt); do
-        	cat $i >> HOUSE_OF_THE_DOCKERS.txt;
-	done
-
-# Remove temporary file
+for i in $(cat temp.txt); do
+    cat $i >> HOUSE_OF_THE_DOCKERS.txt
+done
 rm temp.txt
 
 # Last round
-# Loop over docker3.txt
-for i in  $(ls /home/student/j.bordon/sorted_docker3.txt);do
-        # Read the contents of the current file, take the first 8 lines, 
-        # then select the last 2 lines, extract the last column using awk,
-        # and append to temp.txt 
-        cat $i | head -n 8 | tail -n 2 | awk '{print $NF}' >> temp.txt;
+for i in $(ls ~/projects/house_of_the_dockers/sorted_docker3.txt); do
+    cat $i | head -n 8 | tail -n 2 | awk '{print $NF}' >> temp.txt
 done
 
-	# Read the contents of temp.txt and append to HOUSE_OF_THE_DOCKERS.txt
-	for i in $(cat temp.txt); do
-        	cat $i >> HOUSE_OF_THE_DOCKERS.txt;
-	done
+for i in $(cat temp.txt); do
+    cat $i >> HOUSE_OF_THE_DOCKERS.txt
+done
 rm temp.txt
+
 sleep 3
