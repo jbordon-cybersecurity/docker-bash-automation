0022fc256cc846044a76857019331323de9a87fd
Jose Bordon <40216449@live.napier.ac.uk>
2025-08-28 13:29:42 +0100

    Sanitised script paths for public release

--- step1.sh
+++ step1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-#Author: Jose Bordon
+# Author: Jose Bordon
 #
-#Description: step1.sh script to create, run containers
-#and load the files into the running containers
+# Description: step1.sh script to create, run containers
+# and load the files into the running containers
 #
 
 echo "Creating Docker containers..."
@@ -26,22 +26,20 @@ sleep 2
 
 # Copy files from host to a running docker container
 echo "Loaded files to Docker 1..."
-sudo docker cp /home/student/j.bordon/Docker1/. docker1:/texts | awk '{print $NF}'
+sudo docker cp ~/projects/house_of_the_dockers/Docker1/. docker1:/texts | awk '{print $NF}'
 
 echo "Loaded files to Docker 2..."
-sudo docker cp /home/student/j.bordon/Docker2/. docker2:/texts | awk '{print $NF}'
+sudo docker cp ~/projects/house_of_the_dockers/Docker2/. docker2:/texts | awk '{print $NF}'
 
 echo "Loaded files to Docker 3..."
-sudo docker cp /home/student/j.bordon/Docker3/. docker3:/texts | awk '{print $NF}'
+sudo docker cp ~/projects/house_of_the_dockers/Docker3/. docker3:/texts | awk '{print $NF}'
 
 echo "Sorting files by character length..."
 
 # Execute a command in docker container and output the sorted result to a file
-sudo docker exec docker1 find / -type f -name "*.txt" | wc -c /home/student/j.bordon/Docker1/*.txt > sorted_docker1.txt  
-sudo docker exec docker2 find / -type f -name "*.txt" | wc -c /home/student/j.bordon/Docker2/*.txt | sort -n > sorted_docker2.txt
-sudo docker exec docker3 find / -type f -name "*.txt" | wc -c /home/student/j.bordon/Docker3/*.txt | sort -n > sorted_docker3.txt
+sudo docker exec docker1 find / -type f -name "*.txt" | wc -c ~/projects/house_of_the_dockers/Docker1/*.txt > sorted_docker1.txt  
+sudo docker exec docker2 find / -type f -name "*.txt" | wc -c ~/projects/house_of_the_dockers/Docker2/*.txt | sort -n > sorted_docker2.txt
+sudo docker exec docker3 find / -type f -name "*.txt" | wc -c ~/projects/house_of_the_dockers/Docker3/*.txt | sort -n > sorted_docker3.txt
 
 echo "Files sorted"
 sleep 2
-
-
